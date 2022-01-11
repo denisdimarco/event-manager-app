69b4bf0e3a2259941f52ac4a81a33cf31cc32a51
denisdimarco <denisdimarcoleda@gmail.com>
2022-01-11 03:46:01 -0300

    Created event-items and event-list and css modules

---
+++ components/events/event-item.js
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import classes from './event-item.module.css';
+function EventItem(props) {
+
+    const { title, image, date, location, id } = props;
+
+    const readableDate = new Date(date).toLocaleDateString('en-US', {
+        day: 'numeric',
+        month: 'long',
+        year: "numeric"
+    });
+    const formattedAddress = location.replace(',', '\n');
+    const exploreLink = `/events/${id}`;
+
+    return <li className={classes.item}>
+        <img src={'/'+ image} alt={title}/>
+        <div className={classes.content}>
+            <div className={classes.content}>
+                <h2>{title}</h2>
+            </div>
+            <div className={classes.date}>
+                <time>{readableDate}</time>
+            </div>
+            <div className={classes.actions}>
+                <address>{formattedAddress}</address>
+            </div>
+            <div>
+                <Link href='/'>Explore Event</Link>
+            </div>
+        </div>
+    </li>
+}
+export default EventItem;
